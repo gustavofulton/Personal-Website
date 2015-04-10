0148b0abdfa405d693059ad02cfde7b08c12f037
Gustavo Fulton <ferna194@msu.edu>
2015-04-09 22:42:42 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+
+  require('includes/functions.php');
+
+  echo "<html lang='en'>
+          <head>
+            <meta charset='utf-8'>
+            <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+            <meta name='viewport' content='width=device-width, initial-scale=1'>
+            <title>Gustavo Fulton</title>
+            <link href='includes/bootstrap/css/bootstrap.min.css' rel='stylesheet'>
+            <link href='includes/bootstrap/css/custom.css' rel='stylesheet'>
+            <link rel='shortcut icon' type='image/png' href='images/favicon.jpg'/>
+            <script src='https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js'></script>
+            <script src='https://oss.maxcdn.com/respond/1.4.2/respond.min.js'></script>
+            </head>
+            <body>";
+
+            navLogin();
+
+      echo "  <div class='container'>
+                <div class='row-md text-center'>
+                  <div class='col-md-6 text-left'> 
+                    <h1> This is a test game developed by Gustavo Fulton Fernandes </h1>
+                      <strong><p> O OGame é um jogo de estratégia no espaço onde milhares de jogadores espalhados pelos quatro cantos do mundo, competem ao mesmo tempo. Para conseguir jogar só precisa de um web browser.<p></strong>
+                    <h1> Why would you play? </h1>
+                      <strong> <p> It is scientifially proven that strategy game help develop your brain </p> </strong>
+                    <h1> Got interested? </h1> 
+                      <strong> <p> Register on the side -> </p> </strong> 
+                  </div>
+                  <div class='col-md-6 form-group'> <h4>Login</h4> </div>
+                    <span class='glyphicon glyphicon-user red'></span>
+                    <input type='text' class='input' placeholder='Username' style='border-radius: 9px'>
+                    </br></br>
+                    <span class='glyphicon glyphicon-asterisk red'></span>
+                    <input type='text' class='input' placeholder='Password' style='border-radius: 9px'>
+                    </br> </br>
+                    <div class='btn btn-success'>
+                      <a href='#' />
+                      Submit
+                    </div>
+                </div>
+                <div class='row-md'>
+                  <div class='col-md-12>
+                    <p> Copyright </p>
+                  </div>
+                </div>
+              </div>";
+
+
+
+?>
